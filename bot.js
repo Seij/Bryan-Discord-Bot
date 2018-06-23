31271770f76cd3652185f39b4536de4fddf44fa4
Seij <welsleylui_ackty.bit48@hotmail.com>
2018-06-23 23:08:21 +0800

    Updated bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ client.on("guildCreate", guild => {
   // console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
   // client.user.setActivity(`Serving ${client.guilds.size} servers`);
   client.user.setActivity(`Serving Lady Diana.`);
-// });
+ });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
